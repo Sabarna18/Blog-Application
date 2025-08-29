a7e0bd87fa09daf94f460e065f4da6c3b23e23ab
Sabarna Guha <sabarnaguha1@gmail.com>
2025-08-29 23:28:25 +0530

    Home page updated

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,13 +3,14 @@ import React from "react";
 export default function Button({
     children,
     type = "button",
-    bgColor = "bg-blue-600",
+    bgColor = "bg-black",
     textColor = "text-white",
     className = "",
     ...props
 }) {
     return (
-        <button className={`px-4 py-2 cursor-pointer rounded-lg ${bgColor} ${textColor} ${className}`} {...props}>
+        <button className={`px-4 py-2 cursor-pointer transition duration-500 border-2 hover:bg-slate-800
+         border-transparent hover:border-blue-800 rounded-lg ${bgColor} ${textColor} ${className}`} {...props}>
             {children}
         </button>
     );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,34 +13,58 @@ function Home() {
         })
     }, [])
 
-    if (posts) {
-        if (posts.length === 0) {
-            return (
-                <div className="w-full py-8 mt-4 text-center">
-                    <Container>
-                        <div className="flex flex-wrap">
-                            <div className="p-2 w-full">
-                                <h1 className="text-2xl font-bold hover:text-gray-500">
-                                    Login to read posts
-                                </h1>
-                            </div>
-                        </div>
-                    </Container>
-                </div>
-            )
-        }
-    }
+    // if (posts) {
+    //     if (posts.length === 0) {
+    //         return (
+    //             <div className="w-full bg-[url('https://media.istockphoto.com/id/1570132128/vector/abstract-gradient-black-and-gray-stripe-lines-decorative-artwork-overalpping-with-luxury.jpg?s=612x612&w=0&k=20&c=3mgA_9c28y1-8es4nj-O3z4u0CWkr_jGAXfOXYwyWfg=')] py-8 mt-4 text-center">
+    //                 <Container>
+    //                     <div className="flex flex-wrap">
+    //                         <div className="p-2 w-full">
+    //                             <div className="w-full bg-white/80 border border-black/10 rounded-xl p-10">
+    //                                 <h2 className="text-2xl font-bold mb-4">Welcome to the CORE</h2>
+    //                                 <p className="text-black/60">This is a space where ideas, stories, and knowledge come together. Whether you’re here to learn something new, explore fresh perspectives, or simply enjoy good writing, you’ll find content crafted with clarity and purpose.
+
+    //                                     Our blog is designed to be simple, distraction-free, and reader-friendly—because we believe words should speak louder than clutter. Each post is curated to bring value, whether it’s a quick tip, a deep dive, or a personal reflection.
+
+    //                                     Stay tuned for regular updates, share your thoughts in the comments, and be part of a growing community of curious minds.</p>
+    //                             </div>
+    //                         </div>
+    //                     </div>
+    //                 </Container>
+    //             </div>
+    //         )
+    //     }
+    // }
+    // return (
+    //     <div className='w-full py-8'>
+    //         <Container>
+    //             <div className='flex flex-wrap'>
+    //                 {posts.map((post) => (
+    //                     <div key={post.$id} className='p-2 w-1/4'>
+    //                         <PostCard {...post} />
+    //                     </div>
+    //                 ))}
+    //             </div>
+    //             <div>Welcomee to core world</div>
+    //         </Container>
+    //     </div>
+    // )
+
     return (
-        <div className='w-full py-8'>
+        <div className="w-full bg-[url('https://media.istockphoto.com/id/1570132128/vector/abstract-gradient-black-and-gray-stripe-lines-decorative-artwork-overalpping-with-luxury.jpg?s=612x612&w=0&k=20&c=3mgA_9c28y1-8es4nj-O3z4u0CWkr_jGAXfOXYwyWfg=')] py-8 mt-4 text-center">
             <Container>
-                <div className='flex flex-wrap'>
-                    {posts.map((post) => (
-                        <div key={post.$id} className='p-2 w-1/4'>
-                            <PostCard {...post} />
+                <div className="flex flex-wrap">
+                    <div className="p-2 w-full">
+                        <div className="w-full bg-white/80 border border-black/10 rounded-xl p-10">
+                            <h2 className="text-2xl font-bold mb-4">Welcome to the CORE</h2>
+                            <p className="text-black/60">This is a space where ideas, stories, and knowledge come together. Whether you’re here to learn something new, explore fresh perspectives, or simply enjoy good writing, you’ll find content crafted with clarity and purpose.
+
+                                Our blog is designed to be simple, distraction-free, and reader-friendly—because we believe words should speak louder than clutter. Each post is curated to bring value, whether it’s a quick tip, a deep dive, or a personal reflection.
+
+                                Stay tuned for regular updates, share your thoughts in the comments, and be part of a growing community of curious minds.</p>
                         </div>
-                    ))}
+                    </div>
                 </div>
-                <div>Welcomee to core world</div>
             </Container>
         </div>
     )
